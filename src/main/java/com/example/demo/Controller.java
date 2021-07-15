103bfbe7701d5c0ed01aa7acc9c8533852ace0af
Georgi Staykov <georgistaykov@Georgis-MacBook-Pro-2.local>
2021-07-15 16:58:22 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import io.polygon.kotlin.sdk.rest.PolygonRestClient;
+import io.polygon.kotlin.sdk.rest.reference.MarketsDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/test")
+    public String testPolygon()
+    {
+        String polygonKey = "COrjZBVMfKHQpK2S9EfW0ngvNgxpgWow";
+        PolygonRestClient client = new PolygonRestClient(polygonKey);
+        MarketsDTO markets = client.getReferenceClient().getSupportedMarketsBlocking();
+        System.out.println("Got markets synchronously: " + markets.toString());
+
+        return "ok.";
+    }
+}
